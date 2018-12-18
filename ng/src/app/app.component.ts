563bfd17379019a65befee6a7f8defde46f1f811
Ignacio Buioli <ibuioli@gmail.com>
2018-12-17 23:28:26 -0300

    style: switch to boostrap 4 for responsive, SCSS & change of logic

--- ng/src/app/app.component.ts
+++ ng/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { FilesService } from './services/files.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   public title:string = 'app works';
