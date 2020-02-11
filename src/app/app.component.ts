df580fbfbecb2568f63633fb6f1b99abca0b5097
Ignacio Buioli <ibuioli@gmail.com>
2020-02-11 01:44:30 -0300

    feat: improve performance on keyDown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,25 +41,27 @@ export class AppComponent implements AfterViewInit {
     // Listener TextArea
     this.renderer.listen(this.p5.nativeElement, 'keyup', (e) => {
       // Get Value
-      const txt = (<HTMLInputElement>this.p5.nativeElement).value;
-      const hlP5 = this.hl.highlightP5(txt);
-      this.fp5.nativeElement.innerHTML = hlP5 + '&#13;&#10;&#13;&#10;';
+      if (e.keyCode < 37 || e.keyCode > 40) {
+        const txt = (<HTMLInputElement>this.p5.nativeElement).value;
+        const hlP5 = this.hl.highlightP5(txt);
+        this.fp5.nativeElement.innerHTML = hlP5 + '&#13;&#10;&#13;&#10;';
 
-      const reset = this.conversor.reset(txt);
-      const convers = this.conversor.conversor(reset, this.ofv);
-      this.p5v = this.conversor.p5ver(reset);
+        const reset = this.conversor.reset(txt);
+        const convers = this.conversor.conversor(reset, this.ofv);
+        this.p5v = this.conversor.p5ver(reset);
 
-      const apph = this.conversor.ofApph(convers);
-      this.apph = apph;
-      this.of3.nativeElement.innerHTML = this.hl.highlightC(apph);
+        const apph = this.conversor.ofApph(convers);
+        this.apph = apph;
+        this.of3.nativeElement.innerHTML = this.hl.highlightC(apph);
 
-      const maincpp = this.conversor.maincpp(convers);
-      this.main = maincpp;
-      this.of2.nativeElement.innerHTML = this.hl.highlightC(maincpp);
+        const maincpp = this.conversor.maincpp(convers);
+        this.main = maincpp;
+        this.of2.nativeElement.innerHTML = this.hl.highlightC(maincpp);
 
-      const appcpp = this.conversor.ofAppcpp(convers);
-      this.app = appcpp;
-      this.of.nativeElement.innerHTML = this.hl.highlightC(appcpp);
+        const appcpp = this.conversor.ofAppcpp(convers);
+        this.app = appcpp;
+        this.of.nativeElement.innerHTML = this.hl.highlightC(appcpp);
+      }
     });
     // Get Tab Event
     this.renderer.listen(this.p5.nativeElement, 'keydown', (e) => {
