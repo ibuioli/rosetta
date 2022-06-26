791a25041edf3fe54c2d6e694c5cab91a578483d
Ignacio Buioli <ibuioli@gmail.com>
2022-06-26 18:06:08 -0300

    feat: update to NG14

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,7 +18,7 @@
  * BROWSER POLYFILLS
  */
 
-import 'core-js/proposals/reflect-metadata';
+// import 'core-js/proposals/reflect-metadata';
 
 /** IE9, IE10 and IE11 requires all of the following polyfills. **/
 // import 'core-js/es6/symbol';
